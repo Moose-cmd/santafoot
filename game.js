deffb9f505ed57a350b5fafd853b80535bd6ecc5
Alek <alekmani1@gmail.com>
2018-12-14 18:28:35 -0500

    we added images and scenes *(*(*(*)

--- game.js
+++ game.js
@@ -1,5 +1,29 @@
-var BootScene = new Phaser.Class();
-var WorldScene = new Phaser.Class();
+var BootScene = new Phaser.Class({
+
+    initalize: function BootScene() {
+        Phaser.Scene.call(this, {key: 'BootScene'});
+    },
+
+    preload: function() {
+        this.load.image('tiles', 'assets/spritesheet.png');
+        this.load.tilemapTiledJSON('map', 'assets/map.json');
+        this.load.spritesheet('player', 'assets/characterwithnoheart.png');
+
+    },
+    create: function() {
+        this.scene.start('WorldScene');
+
+
+
+    }
+
+});
+
+var WorldScene = new Phaser.Class({
+    initalize: function BootScene() {
+        Phaser.Scene.call(this, {key: 'WorldScene'});
+    }
+});
 
 var config = {
     type: phaser.SVG_MARKER_ORIENT_AUTO
